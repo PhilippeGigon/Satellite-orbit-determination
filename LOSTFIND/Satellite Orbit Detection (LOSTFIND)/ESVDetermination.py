b14d9622e12e07ae59173ca42d7a01e0b52807d0
AymericLabarbe <aymeric.labarbe@epfl.ch>
2022-03-07 10:24:15 +0100

    Calculation of R

--- LOSTFIND/Satellite Orbit Detection (LOSTFIND)/ESVDetermination.py
+++ LOSTFIND/Satellite Orbit Detection (LOSTFIND)/ESVDetermination.py
@@ -1,20 +1,38 @@
 from IOD_class import *
 from numpy import *
 import os
+import ephem
+import math
 
 
 def get_R():
     '''This function reads the telescopes GPS position
     and then finds the coordinates in the absolute coordinate system'''
-    ################################################################
-    ########################TO DO###################################
-    ################################################################
     script_dir = os.path.dirname(__file__)  # Location of python script
     rel_path = "TelescopeLocation.txt"  # Name of .txt
     abs_file_path = os.path.join(script_dir, rel_path)
     Data = open(abs_file_path, "r")
     Station_Coordinates = Data.readlines()
+    ################################################################
+    ########################Rick part, may be wrong###################################
+    ################################################################
+    Re = 6378137  #Rayon equatorial Terre
+    f =  0.003353  #oblateness 
+    lat = Station_Coordinates[0]; #read latitudes GPS coordinates
+    H = Station_Coordinates[2]; #read altitudes from GPS or google map
+    station = ephem.Observer()
+    station.lon = Station_Coordinates[0]  #should be a str i think
+    station.lat = Station_Coordinates[1]
+    station.date = Station_Coordinates[3]
+    time  = station.sidereal_time()
+    R = ((Re/(math.sqrt(1-(2*f-f*f)*math.sin(lat)*math.sin(lat)))+H)*math.cos(lat)*math.cos(time),
+		(Re/(math.sqrt(1-(2*f-f*f)*math.sin(lat)*math.sin(lat)))+H)*math.cos(lat)*math.sin(time),
+		(Re*(1-f)*(1-f)/(math.sqrt(1-(2*f-f*f)*math.sin(lat)*math.sin(lat)))+H)*math.sin(lat))               
+    ################################################################
+    #############################################################
+    ###############################################################
     Data.close()
+    return R
 
 
 def find_r(iodset):
