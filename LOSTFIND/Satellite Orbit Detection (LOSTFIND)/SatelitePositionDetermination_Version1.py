b746bbc29d95ca8b58eaa1d28c9ed5bd98fc3acf
Philippe <philippe.gigon@epfl.ch>
2022-03-21 08:28:51 +0100

    ..

--- LOSTFIND/Satellite Orbit Detection (LOSTFIND)/SatelitePositionDetermination_Version1.py
+++ LOSTFIND/Satellite Orbit Detection (LOSTFIND)/SatelitePositionDetermination_Version1.py
@@ -5,28 +5,7 @@ import ephem
 import datetime
 import math
 import numpy as np
-
-
-def get_R(epoch_time, lat, H):
-    ################################################################
-    ########################Rick part, may be wrong###################################
-    ################################################################
-    Re = 6378137  # Equatorial earth radius in meter
-    f = 0.003353  # oblateness
-    # read latitudes GPS coordinates
-
-    station = ephem.Observer()
-    date_time = datetime.datetime.fromtimestamp(epoch_time)
-    station.date = date_time
-    time = station.sidereal_time()
-    R = ((Re/(math.sqrt(1-(2*f-f*f)*math.sin(lat)*math.sin(lat)))+H)*math.cos(lat)*math.cos(time),
-         (Re/(math.sqrt(1-(2*f-f*f)*math.sin(lat)*math.sin(lat)))+H) *
-         math.cos(lat)*math.sin(time),
-         (Re*(1-f)*(1-f)/(math.sqrt(1-(2*f-f*f)*math.sin(lat)*math.sin(lat)))+H)*math.sin(lat))
-    ################################################################
-    #############################################################
-    ###############################################################
-    return R
+from StationPositionVector import *
 
 
 def find_rho(N1, N3, D, D11, D12, D13, D21, D22, D23, D31, D32, D33):
