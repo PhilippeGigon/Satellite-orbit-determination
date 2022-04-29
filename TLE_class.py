ed93ed3cbb19122ffcbaed5d6eeb9d86cd2fc1f8
AnnaSchwab <anna.schwab@epfl.ch>
2022-04-29 09:02:01 +0200

    error eccentricity

--- TLE_class.py
+++ TLE_class.py
@@ -56,12 +56,8 @@ class TLE:
         if N[1] < 0:
             Omega = 360 - Omega
 
-        # eccentricity - old variant
-        #ev = 1/constants.mu * ((vn**2 - constants.mu/rn)*self.r - rn*vr*self.v)
-        #e = np.linalg.norm(ev)
-
         # eccentricity - new variant
-        ev = 1/constants.mu * np.dot(self.v, h) - 1/rn * self.r
+        ev = 1/constants.mu * np.linalg.cross(self.v, h) - 1/rn * self.r
         e = np.linalg.norm(ev)
 
         # argument of perigee
