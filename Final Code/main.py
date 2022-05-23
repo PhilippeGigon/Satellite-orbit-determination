97631a08edcd9de84fdcb8dca0e2f7fc1beb64be
Philippe <philippe.gigon@epfl.ch>
2022-05-23 16:07:27 +0200

    small modifications

--- Final Code/main.py
+++ Final Code/main.py
@@ -1,4 +1,5 @@
 import Gauss_algorithm
+import Laplace_algorithm
 from IOD_class import *
 from TLE_class import *
 import math
@@ -9,23 +10,27 @@ import numpy as np
 def main():
     # Creates three IOD
     IODset = np.array(Create_IODs())
-
-    # Gauss orbit determination
+###############################################################
+# Computes speed and position of satellite with Gauss algorithm
+###############################################################
     # precision
     prec = 0.0001
-    # maximal iterations for Gauss algorithm
+    # maximal iterations
     itmax = 1000
-
-    # Computes speed and position of satellite
     r2, v2 = Gauss_algorithm.find_r(IODset, itmax, prec, True)
+###############################################################
+# Computes speed and position of satellite with Laplace algorithm
+###############################################################
+    r2Lapl, v2Lapl = Laplace_algorithm(IODset, 2)
 
+###############################################################
+# Computes the TLE
+###############################################################
     # Corresponding observatioPositionn time
     t2 = IODset[1].get_time()
     TLEinput = [r2, v2, t2]
-
     # Create TLE
     tle = TLE(TLEinput)
-
     # Create TLE file
     tle.TLE_format()
 
